8de80097595fa1175d14c9da2c988416d2d7a953
ntkchinh <ngokieuchinh89@gmail.com>
2021-01-22 23:15:00 +0700

    register  transformer_tall9 hparams

--- t2t_trainer.py
+++ t2t_trainer.py
@@ -6,9 +6,17 @@ from __future__ import print_function
 
 from tensor2tensor.bin import t2t_trainer
 from tensor2tensor.models import transformer
+from tensor2tensor.utils import registry
 import problems
 import tensorflow as tf
 
+
+@registry.register_hparams
+def transformer_tall9():
+  hparams = transformer.transformer_tall()
+  hparams.num_hidden_layers = 9
+  return hparams
+  
 
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
