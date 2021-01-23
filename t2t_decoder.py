84afddf904aedc8f7e2dfb108968359e7804092e
ntkchinh <58228224+ntkchinh@users.noreply.github.com>
2021-01-24 03:52:31 +0700

    Update t2t_decoder.py

--- t2t_decoder.py
+++ t2t_decoder.py
@@ -13,8 +13,11 @@ from tensor2tensor.utils import registry
 
 @registry.register_hparams
 def transformer_tall9():
-  hparams = transformer.transformer_tall()
-  hparams.num_hidden_layers = 9
+  hparams = transformer.transformer_big()
+  hparams.hidden_size = 768
+  hparams.filter_size = 3072
+  hparams.num_hidden_layers = 12
+  hparams.num_heads = 12
   return hparams
 
 
