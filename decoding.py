8556499f4d74f408c70a61b3d0ba7f14645e3670
ntkchinh <ngokieuchinh89@gmail.com>
2021-03-24 10:40:29 +0700

    Update decoding.py

--- decoding.py
+++ decoding.py
@@ -365,8 +365,10 @@ def decode_from_file_fn(estimator,
   tf.logging.info("Writing decodes into %s" % decode_filename)
   outfile = tf.gfile.Open(decode_filename, "w")
   for index in range(len(sorted_inputs)):
-    import pdb; pdb.set_trace()
-    output = decodes[sorted_keys[index]].replace('\n', ' ')
+    special_chars = ["\a", "\t", "\n", "\f", "\r", "\b"]
+    output = decodes[sorted_keys[index]]
+    for c in special_chars:
+      output = output.replace(c, ' ')
     try:
       outfile.write("%s%s" % (output, decode_hp.delimiter))
     except:
