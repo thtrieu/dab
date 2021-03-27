8a2ca8714d9bd0d9efc7f7deca7439d62a86dcfe
ntkchinh <ngokieuchinh89@gmail.com>
2021-03-27 11:11:46 +0700

    Update t2t_saver.py

--- t2t_saver.py
+++ t2t_saver.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
+from tensorflow.keras import layers
 from tensor2tensor.models import transformer
 import problems
 import tensorflow as tf
@@ -17,7 +17,7 @@ flags = tf.flags
 FLAGS = flags.FLAGS
 
 if __name__ == '__main__':
-  tf.logging.set_verbosity(tf.logging.INFO)
+  # tf.logging.set_verbosity(tf.logging.INFO)
   # tf.app.run(t2t_decoder.main)
   trainer_lib.set_random_seed(FLAGS.random_seed)
 
@@ -34,6 +34,16 @@ if __name__ == '__main__':
       decode_hparams=decode_hp
   )
 
+  import pdb; pdb.set_trace()
   model.initialize_from_ckpt(FLAGS.checkpoint_path or FLAGS.output_dir)
+  
+  # features = {
+  #         "inputs": layers.Input(shape=[1, 128]),
+  #         "targets": layers.Input(shape=[1, 128]),
+  #         "target_space_id": pass
+  # }
 
-  model.save('Translation_Vien_Base1m')
+  # predictions, _ = this_model(features)
+
+  # model = keras.Model(inputs, predictions)
+  # model.save('Translation_Vien_Base1m')
