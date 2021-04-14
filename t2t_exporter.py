990370fd0371a0f510ef0bbc67fcd1d1d3c37971
ntkchinh <ngokieuchinh89@gmail.com>
2021-04-14 21:13:43 +0700

    Update t2t_exporter.py
    registry transformer tall9

--- t2t_exporter.py
+++ t2t_exporter.py
@@ -3,14 +3,21 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from tensor2tensor.utils import registry
 
 
 from tensor2tensor.serving import export
-
 import tensorflow as tf
-
 import problems
 
+@registry.register_hparams
+def transformer_tall9():
+  hparams = transformer.transformer_big()
+  hparams.hidden_size = 768
+  hparams.filter_size = 3072
+  hparams.num_hidden_layers = 9
+  hparams.num_heads = 12
+  return hparams
 
 def main(argv):
   export.main(argv)
