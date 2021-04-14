d2a215a8159366b92519f12cdfd79650a69db7c9
ntkchinh <ngokieuchinh89@gmail.com>
2021-04-14 21:16:20 +0700

    Update t2t_exporter.py

--- t2t_exporter.py
+++ t2t_exporter.py
@@ -3,6 +3,8 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+from tensor2tensor.models import transformer
 from tensor2tensor.utils import registry
 
 
