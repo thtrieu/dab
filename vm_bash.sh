e10cc26cca2ced1894b024506b3216f5a8b8d1de
ntkchinh <ngokieuchinh89@gmail.com>
2021-05-16 13:03:45 +0700

    Update vm_bash.sh

--- vm_bash.sh
+++ vm_bash.sh
@@ -55,6 +55,13 @@ export name='translate-'
 vm_num=$1
 count=$2
 
+export train_data_dir=gs://best_vi_translation/data/translate_class11_pure_envi_iwslt32k
+export problem=translate_class11_pure_envi_iwslt32k
+export hparams_set=transformer_tall9
+
+export ckpt_dir=gs://best_vi_translation/checkpoints/translate_class11_pure_envi_tall9_2m/SAVE
+export ckpt_path=gs://best_vi_translation/checkpoints/translate_class11_pure_envi_tall9_2m/SAVE/model.ckpt-500000
+
 # for i in {0..19}; do  
 i=0
 tpu_translate $vm_num $count $i $name & 
