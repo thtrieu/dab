50eeb42e90356aecb488dd910ec6a31b15c8c314
ntkchinh <ngokieuchinh89@gmail.com>
2021-05-17 22:11:46 +0700

    Update vm_bash.sh

--- vm_bash.sh
+++ vm_bash.sh
@@ -3,11 +3,14 @@ translate() {
     tpu_num=$1
     tpu_work_num=$2
     j=$3
+    tpu_name=$4
+    i=$5
+    
     # file_numb=$((tpu_num * tpu_work_num + j))
-    file_addr=$((tpu_num * tpu_work_num + j))
+    # file_addr=$((tpu_num * tpu_work_num + j))
+    file_addr=$((i * tpu_work_num + j))
     file_numb="${left[file_addr]}"
     
-    tpu_name=$4
     echo 'file_numb' $file_numb
     echo 'tpu_name' $tpu_name
 
@@ -29,7 +32,7 @@ translate() {
     --data_dir=$train_data_dir --problem=$problem \
     --hparams_set=$hparams_set \
     --model=transformer \
-    --decode_hparams="beam_size=4,alpha=0.6,log_results=False,return_beams=True" \
+    --decode_hparams="beam_size=4alpha=0.6log_results=Falsereturn_beams=True" \
     --decode_from_file=$decode_from_file \
     --decode_to_file=$decode_to_file 
     
@@ -44,11 +47,11 @@ tpu_translate(){
     tpu_num=$((vm_num * count + i))
     tpu_name=$name$tpu_num
     # tpu_work_num=10
-    tpu_work_num=1
+    tpu_work_num=2
     
     for j in {0..1}; do
     # j=0
-        translate $tpu_num $tpu_work_num $j $tpu_name
+        translate $tpu_num $tpu_work_num $j $tpu_name $i
     done    
     echo 'done on tpu' $tpu_num
 }
@@ -57,7 +60,7 @@ tpu_translate(){
 export name='translate-'
 
 vm_num=$1  ##{0..4}
-count=10
+count=7
 
 
 export train_data_dir=gs://best_vi_translation/data/translate_class11_pure_envi_iwslt32k
@@ -72,10 +75,13 @@ gcloud config set project ${PROJECT_ID}
 gcloud beta services identity create --service tpu.googleapis.com --project $PROJECT_ID
 gcloud auth application-default login
 
-left=(180 181 182 183 184 185 186 187 188 189 369 690 691 
-        692 693 694 695 696 697 698 699 736 737 738 739)
+# left=(180 181 182 183 184 185 186 187 188 189 369 690 691 
+#         692 693 694 695 696 697 698 699 736 737 738 739)
+# 14 left
+left=(690 691 692 693 694 695 696 
+      697 698 699 736 737 738 739)
 
 # for ((i=0;i<$count;i++)); do  # i in {$count_0..$count_1}; do  
-for i in {0..9}; do
+for i in {0..6}; do
     tpu_translate $vm_num $count $i $name & 
 done
