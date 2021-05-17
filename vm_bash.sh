7339718ded062ad137e284bad1cd90c958ba18a8
ntkchinh <ngokieuchinh89@gmail.com>
2021-05-17 18:14:44 +0700

    Update vm_bash.sh

--- vm_bash.sh
+++ vm_bash.sh
@@ -3,7 +3,10 @@ translate() {
     tpu_num=$1
     tpu_work_num=$2
     j=$3
-    file_numb=$((tpu_num * tpu_work_num + j))
+    # file_numb=$((tpu_num * tpu_work_num + j))
+    file_addr=$((tpu_num * tpu_work_num + j))
+    file_numb="${left[file_addr]}"
+    
     tpu_name=$4
     echo 'file_numb' $file_numb
     echo 'tpu_name' $tpu_name
@@ -16,22 +19,19 @@ translate() {
     
     echo 'decode from ' $decode_from_file
     echo 'decode to ' $decode_to_file
-    if [ -f "$decode_to_file" ]; then
-        echo "$decode_to_file exists."
-    else 
-        python3 t2t_decoder.py \
-        --alsologtostderr \
-        --output_dir=$ckpt_dir \
-        --checkpoint_path=$ckpt_path \
-        --use_tpu \
-        --cloud_tpu_name=$tpu_name \
-        --data_dir=$train_data_dir --problem=$problem \
-        --hparams_set=$hparams_set \
-        --model=transformer \
-        --decode_hparams="beam_size=4,alpha=0.6,log_results=False,return_beams=True" \
-        --decode_from_file=$decode_from_file \
-        --decode_to_file=$decode_to_file 
-    fi
+     
+    python3 t2t_decoder.py \
+    --alsologtostderr \
+    --output_dir=$ckpt_dir \
+    --checkpoint_path=$ckpt_path \
+    --use_tpu \
+    --cloud_tpu_name=$tpu_name \
+    --data_dir=$train_data_dir --problem=$problem \
+    --hparams_set=$hparams_set \
+    --model=transformer \
+    --decode_hparams="beam_size=4,alpha=0.6,log_results=False,return_beams=True" \
+    --decode_from_file=$decode_from_file \
+    --decode_to_file=$decode_to_file 
     
 }
 
@@ -43,9 +43,11 @@ tpu_translate(){
 
     tpu_num=$((vm_num * count + i))
     tpu_name=$name$tpu_num
-    tpu_work_num=10
+    # tpu_work_num=10
+    tpu_work_num=1
     
-    for j in {0..9}; do
+    for j in {0..1}; do
+    # j=0
         translate $tpu_num $tpu_work_num $j $tpu_name
     done    
     echo 'done on tpu' $tpu_num
@@ -55,7 +57,7 @@ tpu_translate(){
 export name='translate-'
 
 vm_num=$1  ##{0..4}
-count=$2
+count=10
 
 
 export train_data_dir=gs://best_vi_translation/data/translate_class11_pure_envi_iwslt32k
@@ -70,6 +72,10 @@ gcloud config set project ${PROJECT_ID}
 gcloud beta services identity create --service tpu.googleapis.com --project $PROJECT_ID
 gcloud auth application-default login
 
-for ((i=0;i<$count;i++)); do  # i in {$count_0..$count_1}; do  
+left=[180 181 182 183 184 185 186 187 188 189 369 690 691 
+        692 693 694 695 696 697 698 699 736 737 738 739]
+
+# for ((i=0;i<$count;i++)); do  # i in {$count_0..$count_1}; do  
+for i in {0..9}; do
     tpu_translate $vm_num $count $i $name & 
 done
