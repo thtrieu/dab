1490d4a44abd45df8efbe2e8ad0f9443330ee8f3
thtrieu <thtrieu@apcs.vn>
2021-07-22 10:19:46 -0400

    Update t2t_trainer.py

--- t2t_trainer.py
+++ t2t_trainer.py
@@ -11,6 +11,45 @@ import problems
 import tensorflow as tf
 
 
+@registry.register_model
+class TransformerExtraTokenToDecoder(transformer.Transformer):
+
+  def __init__(self, *args, **kwargs):
+    super(TransformerExtraTokenToDecoder, self).__init__(*args, **kwargs)
+
+    def _prepare_extra_token_decoder_fn(targets, hparams, features):
+      decoder_input, decoder_self_attention_bias = transformer.transformer_prepare_decoder(
+          targets, hparams, features)
+      # [batch, length, hidden_dim], [None, 1, length, length]
+
+      batch_size = decoder_input.shape[0]
+      hidden_dim = hparams.hidden_size
+      num_extras = hparams.extra_tokens
+      extra_tokens = tf.get_variable(
+          'extra_tokens', [1, num_extras, hidden_dim],
+          initializer=tf.random_normal_initializer(0.0, hidden_dim**-0.5))
+      extra_tokens = tf.repeat(
+          extra_tokens, batch_size, axis=0)  # [batch, num_extras, hidden_dim]
+      decoder_input = tf.concat(
+          [extra_tokens, decoder_input], axis=1)  # [batch, num_extras+len, hidden_dim]
+
+      batch_size = decoder_self_attention_bias.shape[0]
+
+      decoder_self_attention_bias = tf.pad(
+          decoder_self_attention_bias,
+          [[0, 0], [0, 0], [num_extras, 0], [num_extras, 0]]
+      )
+      return decoder_input, decoder_self_attention_bias
+
+    self._prepare_decoder_fn = _prepare_extra_token_decoder_fn
+  
+  def decode(self, *args, **kwargs):
+    hparams = self._haprams
+    decoder_output = super(
+        TransformerExtraTokenToDecoder, self).decode(*args, **kwargs)
+    return decoder_output[:, hparams.num_extra:, :]
+
+
 @registry.register_hparams
 def transformer_tall9():
   hparams = transformer.transformer_big()
@@ -18,6 +57,7 @@ def transformer_tall9():
   hparams.filter_size = 3072
   hparams.num_hidden_layers = 9
   hparams.num_heads = 12
+  hparams.num_extras = 8
   return hparams
   
 
